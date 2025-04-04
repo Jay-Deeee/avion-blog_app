59c95dd4229a8ee8b2580c33eb1eb23c5f7950e8
Jay-Deeee <144668463+Jay-Deeee@users.noreply.github.com>
2025-04-04 19:32:49 +0800

    fixed show and edit functions for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,21 @@
 class CommentsController < ApplicationController
+  before_action :set_article
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
   def index
-    @article = Article.find(params[:article_id])
     @comments = @article.comments
   end
 
-  def show
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-  end
+  def show; end
 
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
-  def edit
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-  end
+  def edit; end
   
   def update
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-
     if @comment.update(comment_params)
       redirect_to @article, notice: "Comment has been updated."
     else
@@ -32,8 +24,6 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
     @comment.destroy
 
     redirect_to @article, status: :see_other
@@ -42,5 +32,13 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
+
+  def set_comment
+    @comment = @article.comments.find(params[:id])
   end
 end
