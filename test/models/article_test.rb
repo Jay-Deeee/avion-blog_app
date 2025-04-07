ed9ca91f746dfacf30cbe7af81869efb6ffa7d24
Jay-Deeee <144668463+Jay-Deeee@users.noreply.github.com>
2025-04-07 20:54:32 +0800

    Added model testing for articles and comments

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save an article with nil title" do
+    article = Article.new
+    article.title = nil
+    article.body = "I wanna be the very best"
+    assert article.save # should return false
+  end
+
+  test "should not save an article with short body" do
+    article = Article.new(title: "Letter a", body: "a")
+    assert article.save # should return false
+  end
+
+  test "should not save an article with nil body" do
+    article = Article.new(title: "nil body", body: nil)
+    assert_not article.save # should return true
+  end
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save a comment without a commenter" do
+  article = Article.create(title:"First", body:"body of the first article.")
+    comment = Comment.new(commenter: nil, body: "Hello there", article_id: article.id)
+    assert_not comment.save
+  end
+
+  test "should not save a comment without a body" do
+    article = Article.create(title:"First", body:"body of the first article.")
+    comment = Comment.new(commenter: "Jino", body: "Hello there", article_id: article.id)
+    assert comment.save
+  end
 end
