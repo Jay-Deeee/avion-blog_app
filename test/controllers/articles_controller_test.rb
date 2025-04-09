27b76d6629f9007d46591cc331d16e2e663f78fa
Jay-Deeee <144668463+Jay-Deeee@users.noreply.github.com>
2025-04-09 17:53:59 +0800

    Installed Devise gem and added authentication to app

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get articles index" do
+    get '/articles'
+    assert_response :success # can also be :ok and 200
+  end
+
+  test "should get new page" do
+    get '/articles/new'
+    assert_response :ok
+  end
+
+  test "should create an article" do
+    post articles_path, params: { article: {title: "New Title", body: "New body for article"} }
+    assert_response :redirect
+  end
+
+  # fix these tests
+  test "should be able to edit an article" do
+    post articles_path, params: { article: {title: "New Title", body: "New body for article"} }
+    # get edit_articles_path, params: { article: {title: "Edited Title", body: "Edited body for the article"} }
+    assert_response :redirect
+  end
+
+  test "should be able to delete an article" do
+    # post articles_path, params: { article: {title: "New Title", body: "New body for article"} }
+    # assert_response :redirect
+  end
 end
